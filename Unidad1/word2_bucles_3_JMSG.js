f8d2d1767546abd8f9cd83bddd1eb731ab02243d
llllJokerllll <Yosi15@gmail.com>
2019-10-21 20:32:58 +0200

    update

--- Unidad1/word2_bucles_3_JMSG.js
+++ Unidad1/word2_bucles_3_JMSG.js
@@ -1,7 +1,24 @@
-var num = parseInt(prompt("Por favor, introduzca un número entero"));
+var num = parseInt( prompt("Por favor, introduzca un número entero" ) );
+var cadena = [];
 
-for (let i = 1; i <= num; i++) {
-    for (let j = 1; j <= num; j++) {
+for ( let i = 1; i <= num; i++ ) {
+    for ( let j = 0; j <= i; j++ ) {
+        if ( ( j == 1 && i == 1 ) || ( j == 1 && i == 2 ) ) {
+            cadena.push( j.toString() + "/" + i.toString() );
+        }
+        if ( j != 0 && j != i && (j / i != 0.5 )) {
+            cadena.push( j.toString() + "/" + i.toString() );
+            
+        }
         
     }
 }
+var result = cadena.sort();
+
+/* for ( let x = 1; x < cadena.length; x++ ) {
+    if ( j == 0 || j == i ) {
+        cadena.splice(x,1);
+        
+    }
+} */
+console.log(result);
