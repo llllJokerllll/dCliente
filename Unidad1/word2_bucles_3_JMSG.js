1526f52b592975a17aff03f218b23daf953d9767
llllJokerllll <Yosi15@gmail.com>
2019-10-22 19:30:20 +0200

    update

--- Unidad1/word2_bucles_3_JMSG.js
+++ Unidad1/word2_bucles_3_JMSG.js
@@ -1,24 +1,42 @@
-var num = parseInt( prompt("Por favor, introduzca un número entero" ) );
-var cadena = [];
+function secuenciaFarey(int n) {
+    int []num = new int[100];
+    int []den = new int[100];
+    int termos = 2;
+    boolean existen_novos_termos = true;
 
-for ( let i = 1; i <= num; i++ ) {
-    for ( let j = 0; j <= i; j++ ) {
-        if ( ( j == 1 && i == 1 ) || ( j == 1 && i == 2 ) ) {
-            cadena.push( j.toString() + "/" + i.toString() );
+    num[0] = 0;
+    den[0] = 1;
+    num[1] = 1;
+    den[1] = 1;
+
+    while (existen_novos_termos) {
+        existen_novos_termos = false;
+        for (int i = 0; i < termos - 1; i++) {
+            if (den[i] + den[i+1] <= n) {
+                inserir_termo(num, den, termos, i);
+                existen_novos_termos = true;
+                termos++;
+            }
         }
-        if ( j != 0 && j != i && (j / i != 0.5 )) {
-            cadena.push( j.toString() + "/" + i.toString() );
-            
+    }
+    mostrar_termos(num, den, termos);
+}
+
+function mostar_termos (int[] num, int[] den, int termos) {
+    for (int i = 0; i < termos; i++) {
+        if (i == 0) {
+            System.out.printf("%d/%d", num[i], den[i]);
+        } else {
+            System.out.printf(", %d/%d", num[i], den[i]);
         }
-        
     }
 }
-var result = cadena.sort();
 
-/* for ( let x = 1; x < cadena.length; x++ ) {
-    if ( j == 0 || j == i ) {
-        cadena.splice(x,1);
-        
+function inserir_termo(int[] num, int[] den, int termos, int i) {
+    for (int j = termos; j > i; j--) {
+        num[j] = num[j - 1];
+        den[j] = den[j - 1];
     }
-} */
-console.log(result);
+    num[i + 1] = num[i] + num[i + 2];
+    den[i + 1] = den[i] + den[i + 2]
+}
