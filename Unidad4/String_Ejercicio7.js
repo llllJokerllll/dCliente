b5fcec75e633aaa97197c284c5ef304f228f1c72
llllJokerllll <Yosi15@gmail.com>
2019-11-18 20:37:11 +0100

    update

---
+++ Unidad4/String_Ejercicio7.js
@@ -0,0 +1,28 @@
+var texto = prompt("Por favor, introduzca su texto a codificar aquí");
+const ABECEDARIO = " ABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789";
+const NUMEROS = "0123456789";
+var textoMayus = texto.toUpperCase();
+var textoCodificado = "";
+var textoSalida = "";
+
+for ( let i = 0; i <= textoMayus.length; i++ ) {
+    for ( let j = 0; j < ABECEDARIO.length; j++ ) {
+        if ( ABECEDARIO.charAt( j ) == ( textoMayus.charAt( i ) ) ) {
+            if ( textoMayus.charAt( i ) == " " ) {
+                textoCodificado += " ";
+            } else if ( textoMayus.charAt( i ) == "Z" ) {
+                textoCodificado += "A";
+            } else if ( textoMayus.charAt( i ) == "9" ) {
+                textoCodificado += "0";    
+            } else {
+            textoCodificado += ABECEDARIO.charAt( j + 1 );
+            }
+        }
+    }
+}
+
+for ( let i = textoCodificado.length; i >= 0; i-- ) {
+    textoSalida += textoCodificado.charAt(i);
+}
+
+console.log( textoSalida );
