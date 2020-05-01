eff6513dc9326ca0b637d05af3ab82221ef0fabb
llllJokerllll <Yosi15@gmail.com>
2020-05-01 15:40:36 +0200

    update

--- Tema5/5_6_cookiesWebStorage/ejercicio1.js
+++ Tema5/5_6_cookiesWebStorage/ejercicio1.js
@@ -2,30 +2,30 @@ window.onload = () => {
     if (typeof(Storage) != "undefined") {
         const id = (dir) => document.getElementById(dir);
         const actualizar = () => {
-            id("saludo").innerHTML = "Hola " + sessionStorage.getItem("nombre") + "!!!";
-            id("contador").innerHTML = "Contador: " + sessionStorage.getItem("numVeces");
+            id("saludo").innerHTML = "Hola " + localStorage.getItem("nombre") + "!!!";
+            id("contador").innerHTML = "Contador: " + localStorage.getItem("numVeces");
         };
         const incrementar = () => {
-            sessionStorage.setItem("numVeces", +sessionStorage.getItem("numVeces") + 1);
+            localStorage.setItem("numVeces", +localStorage.getItem("numVeces") + 1);
             actualizar();
         }
 
         id("incrementar").addEventListener("click", incrementar);
         id("decrementar").addEventListener("click", () => {
-            sessionStorage.setItem("numVeces", sessionStorage.getItem("numVeces") - 1);
+            localStorage.setItem("numVeces", localStorage.getItem("numVeces") - 1);
             actualizar();
         })
         id("logout").addEventListener("click", () => {
-            sessionStorage.setItem("nombre", alert("Se hace cerrado la sesion correctamente"))
-            sessionStorage.clear();
+            localStorage.setItem("nombre", alert("Se hace cerrado la sesion correctamente"))
+            localStorage.clear();
             location.reload();
         })
 
-        if (sessionStorage.getItem("nombre") == null) {
-            sessionStorage.setItem("nombre", prompt("Por favor, introduzca su nombre"));
-            sessionStorage.setItem("numVeces", 0);
+        if (localStorage.getItem("nombre") == null) {
+            localStorage.setItem("nombre", prompt("Por favor, introduzca su nombre"));
+            localStorage.setItem("numVeces", 0);
         } else {
-            alert("Hola " + sessionStorage.getItem("nombre"));
+            alert("Hola " + localStorage.getItem("nombre"));
             incrementar();
         }
 
